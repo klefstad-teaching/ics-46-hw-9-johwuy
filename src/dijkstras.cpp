7c39ddbb0bd65502e43b9c0555c30f63391d11e6
johwuy <johnnyw8@uci.edu>
2025-03-08 21:24:48 +0000

    implement boilerplate for dijkstras

---
+++ src/dijkstras.cpp
@@ -0,0 +1,12 @@
+#include "dijkstras.h"
+
+std::vector<int> dijkstra_shortest_path(const Graph &G, int source,
+                                        vector<int> &previous) {
+    return {};
+}
+std::vector<int> extract_shortest_path(const std::vector<int> & /*distances*/,
+                                       const std::vector<int> &previous,
+                                       int destination) {
+    return {};
+}
+void print_path(const vector<int> &v, int total) {}
